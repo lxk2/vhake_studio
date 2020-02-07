38c2ce505106d5b7d35d7a192d17f5b89b44c2f7
lxk2 <737697366@qq.com>
2020-02-07 21:55:40 +0800

    abc

--- src/utils/statusCode.js
+++ src/utils/statusCode.js
@@ -4,7 +4,7 @@ export const ERROR_CODE = 1
 
 export const ERR_AUTH_CODE = -1
 
-export const DOMAIN = 'http://www.ccb.com:8888'
+export const DOMAIN = 'http://gzsapi.jtlw.com.cn'
 // export const DOMAIN = '';
 
 export const BASE_URL = DOMAIN + '/api'
