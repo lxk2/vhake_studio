ac598de1d858d27f38d16497328bc42224e24642
lxk2 <737697366@qq.com>
2020-02-07 22:15:20 +0800

    abc

--- src/views/feedback/index.vue
+++ src/views/feedback/index.vue
@@ -5,7 +5,7 @@
       minHeight: 100
     }" label="反馈意见" placeholder="请输入反馈意见" ></van-field>
     <van-field label-width="90" v-model="formData.mobile" type="tel" label="电话" placeholder="请输入联系电话" ></van-field>
-    <van-field label-width="90" v-model="formData.mobile" type="text" label="邮箱" placeholder="请输入联系邮箱" ></van-field>
+    <van-field label-width="90" v-model="formData.email" type="text" label="邮箱" placeholder="请输入联系邮箱" ></van-field>
     <div class="open-ccb-box">
       <div class="open-ccb" @click="submitForm">提交</div>
     </div>
