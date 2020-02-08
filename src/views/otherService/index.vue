fe536316d058bc44dcf4bf3428c46f59553cb734
lxk2 <737697366@qq.com>
2020-02-08 18:12:54 +0800

    abc

--- src/views/otherService/index.vue
+++ src/views/otherService/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <van-notice-bar wrapable :scrollable="false" mode="closeable">
+    <!-- <van-notice-bar wrapable :scrollable="false" mode="closeable">
       资料审批盖章或其他有关项目，输入相关信息提交即可
       <br />
       如有任何操作问题可随时联系拨打客服电话或联系您的业务经办人，期待您的体验及建议，晋拓将一如既往地提供优质高效专业的服务！
-    </van-notice-bar>
+    </van-notice-bar> -->
     <div class="container" v-html="netData.content"></div>
   </div>
 </template>
